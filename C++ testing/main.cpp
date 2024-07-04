622ae5c31dd81da834da3f9305ddd25d0dd03422
De Yang <deyang9822@gmail.com>
2024-07-04 13:34:39 +0800

    Initial Commit

---
+++ C++ testing/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++ testing
+//
+//  Created by 的的的羊 on 2024/7/4.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
