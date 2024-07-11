57386a381f40e89372dad245beaf978dbae50490
De Yang <deyang9822@gmail.com>
2024-07-11 13:53:12 +0800

    Smart pointers- first type: unique_ptr update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
-//Topic: Object lifetime
-// Stack: the variable get destroyrd as soon as we get out of the scope.
+#include <memory>// for unique_ptr
+//Topic:Smart Pointers
+// Way to automaticlly store the data on heap and delete outside the scope.
+// No need to use "new" and "delete".
+// First: unique_ptr:  scope pointer, when the pointer is out of the scope, it will be deleted automatically. Can't be copied or shared.
 class Entity{
 public:
     Entity()
@@ -14,29 +17,9 @@ public:
     };
 };
 
-int* CreatArray(){
-    int* array=new int[50];// create a variable on heap
-    return array;
-}
-
-class scopeptr
-{
-private:
-    Entity* m_ptr;
-public:
-    scopeptr(Entity* ptr):m_ptr(ptr)// initualize the m_ptr;
-    {
-    }
-   ~scopeptr(){
-        delete m_ptr; // delete the m_ptr
-    }
-};
-
 int main(){
     {
-        scopeptr e=new Entity() ;
-         
-        int* a=CreatArray();// we can still access the function outside the scope if we build the array on heap.
+        std::unique_ptr<Entity> entity=std::make_unique<Entity>();// By debuging, we can see that the class entitiy is destroyed out of the scope.
     }
     std::cin.get();
         
